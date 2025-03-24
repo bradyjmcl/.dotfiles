840fc7fa92af2bfaf6bfb8dde326a4557a5fa846
Brady McLaughlin <78229080+bradyjmcl@users.noreply.github.com>
2025-03-24 14:52:12 -0400

    Create dotfiler.sh

---
+++ dotfiler.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# copy tmux.conf file
+mkdir -p ~/.config/tmux/plugins/catppuccin
+cp tmux.conf ~/.config/tmux/tmux.conf
+
+# clone catpuccin
+git clone -b v2.1.0 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+
+# clone TPM
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# copy Ghostty conf
+mkdir -p ~/.config/ghostty
+cp ghostty.conf ~/.config/ghostty/config
